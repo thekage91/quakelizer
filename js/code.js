054ea119b3fd7c5a861599a50c90dc51baab8b0c
davide <thekage91@gmail.com>
2016-03-31 11:45:24 +0200

    Enhancement of reactive features

--- js/code.js
+++ js/code.js
@@ -1,23 +1,22 @@
 
 var quakes = Rx.Observable.create(function(observer){
 	window.eqfeed_callback = function(response){
-		console.log("prova")
-		var quakes = response.features;
-
-		quakes.forEach(function(quake){
-			observer.onNext(quake);
-		});
+		// Main Observable emits the response
+		observer.onNext(response);
+		observer.onCompleted();
 	};
 
 	loadJSONP(QUAKE_URL);	
+}).flatMap(function transform(dataset){
+	// Generate ad Observable from Array of features
+	return Rx.Observable.from(dataset.response.features);
 });
 
 quakes.subscribe(function(quake){
 	/* Retrive coordinates and size from quake */
 	var coords = quake.geometry.coordinates;
-
 	/* Size is proportional with magnitude */
-	var size = quake.properties.mag * 1000;
+	var size = quake.properties.mag * 10000;
 
 	L.circle([coords[1], coords[0]], size).addTo(map);
 });
