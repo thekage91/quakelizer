0427b09402895f60a5bd1df8e52b25d332b46341
davide <the--kage91@hotmail.it>
2016-04-01 21:17:32 +0200

    [Features 2] Show popup when mouse over earthquake

--- js/code.js
+++ js/code.js
@@ -1,20 +1,48 @@
 
-var quakes = Rx.DOM.jsonpRequest({
-	url: QUAKE_URL,
-	jsonpCallback: 'eqfeed_callback'
+/**
+* Observable that rapresent the mouse move
+*/
+var mouseMove = Rx.Observable.fromEvent(document, 'mousemove');
+
+
+var quakes = Rx.Observable.interval(5000)
+.flatMap(function(){
+	return Rx.DOM.jsonpRequest({
+		url: QUAKE_URL,
+		jsonpCallback: 'eqfeed_callback'
+	}).retry(3);
 })
 .flatMap(function(result){
 	// Generate ad Observable from Array of features
 	return Rx.Observable.from(result.response.features);
 })
+.distinct(function(quake) { return quake.properties.code })
 .map(function(quake){
 	return {
 		lat: quake.geometry.coordinates[1],
 		lng: quake.geometry.coordinates[0],
-		size: quake.properties.mag * 10000
+		size: quake.properties.mag * 10000,
+		mag: quake.properties.mag
 	};
+})
+.flatMap(function(quake){
+	var marker = L.circle([quake.lat, quake.lng], quake.size).addTo(map);
+	return Rx.Observable.fromEvent(marker, 'mouseover', () => ({ quake: quake, marker: marker }))
+})
+.map(function(data){
+	var popup = data.marker.bindPopup("Magnitudo: " + data.quake.mag).openPopup();
+	return {
+		marker: data.marker,
+		popup: popup,
+		quake: data.quake
+	};
+})
+.flatMap(function(data){
+	return Rx.Observable.fromEvent(data.marker, 'mouseout', () => ( data.popup ));
 });
 
-quakes.subscribe(function(quake){
-	L.circle([quake.lat, quake.lng], quake.size).addTo(map);
+quakes.subscribe(function(popup){
+	
+	popup.closePopup();;
 });
+
